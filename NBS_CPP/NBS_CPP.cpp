f8865fa722daaf974af0ad8adacf386d67c483f1
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-03-30 17:47:54 +0800

    1

--- NBS_CPP/NBS_CPP.cpp
+++ NBS_CPP/NBS_CPP.cpp
@@ -4,6 +4,7 @@
 #include <type_traits>
 
 #include "util/Endian_Helper.hpp"
+#include <vector>
 
 class NBT_DataType
 {
@@ -107,10 +108,32 @@ public:
 		BYTE instrument;	//乐器索引 (0-255)
 		BYTE key;			//音符键位 (0-87, 对应钢琴键)
 		BYTE velocity;		//力度 (0-100, 默认100)
-		BYTE panning;		//声像 (0-200, 0为最左, 200为最右, 100为中央, 默认100)
+		BYTE panning;		//声像 (0-200, 0为最左, 200为最右, 100为中央, 默认100)，使用的时候需-100获得实际值，存储时无符号需要+100
 		SSHORT pitch;		//音高微调 (-32768 to 32767, 默认0)
 	};
 
+	struct Layer
+	{
+		//标识信息
+		SHORT id;			//层索引 (0-based)
+
+		//层属性
+		std::string name;	//层名称
+		BOOL lock;			//是否锁定 (v4+)
+		BYTE volume;		//音量 (0-100)
+		BYTE panning;		//声像 (-100 到 100, 0为中央, 默认100)，使用的时候需-100获得实际值，存储时无符号需要+100
+	};
+
+	struct Instrument
+	{
+		INT id;				//乐器ID（在乐器列表中的索引，从0开始）
+		std::string name;	//乐器名称
+		std::string file;	//自定义音效文件路径（空字符串表示使用默认音色）
+		BYTE pitch;			//音高偏移（默认45，对应C#，范围0-255）
+		BOOL press_key;		//是否按下按键（默认true）
+	};
+
+public:
 #define FAIL_RETURN(func)\
 do\
 {\
@@ -145,7 +168,6 @@ FAIL_RETURN(ReadFromStream((field), fileStream));
 		INT version{};
 		COND_READ(version, song_length == 0, (INT)0);//读入版本号，如果是新版本，则song_length长度为标记值0
 
-
 		//初始化文件头
 		header.version = version;
 
@@ -171,19 +193,82 @@ FAIL_RETURN(ReadFromStream((field), fileStream));
 
 		NORM_READ(header.song_origin);
 
-		COND_READ(header.loop, version >= 4, (BYTE)0);
+		COND_READ(header.loop, version >= 4, (BOOL)false);
 		COND_READ(header.max_loop_count, version >= 4, (BYTE)0);
-		COND_READ(header.loop_start, version >= 4, (BYTE)0);
+		COND_READ(header.loop_start, version >= 4, (SHORT)0);
+	
+		return true;
 	}
 
-	static bool ReadNotes()
+	static bool ReadNotes(const Header &header, std::vector<Note> &listNote, std::fstream &fileStream)
 	{
+		listNote.clear();
 
+		INT cur_tick = -1;
+		while (true)
+		{
+			SHORT jump{};
+			NORM_READ(jump);
+			if (jump == 0)
+			{
+				break;
+			}
+			cur_tick += jump;
+
+			INT cur_layer = -1;
+			while (true)
+			{
+				SHORT jump{};
+				NORM_READ(jump);
+				if (jump == 0)
+				{
+					break;
+				}
+				cur_layer += jump;
+
+				Note note;
+				note.tick = cur_tick;
+				note.layer = cur_layer;
+				NORM_READ(note.instrument);
+				NORM_READ(note.key);
+				COND_READ(note.velocity, header.version >= 4, (BYTE)100);
+				COND_READ(note.panning, header.version >= 4, (BYTE)0);
+				COND_READ(note.pitch, header.version >= 4, (SSHORT)0);
+
+				listNote.push_back(std::move(note));
+			}
+		}
+
+		return true;
 	}
 
+	static bool ReadLayers(const Header &header, std::vector<Layer> &listLayer, std::fstream &fileStream)
+	{
+		listLayer.clear();
+		listLayer.reserve(header.song_layers);
+
+		for (SHORT i = 0; i < header.song_layers; ++i)
+		{
+			Layer layer;
+			layer.id = i;
+			NORM_READ(layer.name);
+			COND_READ(layer.lock, header.version >= 4, (BOOL)false);
+			NORM_READ(layer.volume);
+			COND_READ(layer.panning, header.version >= 2, (BYTE)0);
+
+			listLayer.push_back(std::move(layer));
+		}
+
+		return true;
+	}
+
+	static bool ReadInstruments(const Header &header, std::vector<Instruments> &listInstruments, std::fstream &fileStream)
+	{
+		listInstruments.clear();
 
 
-
+		return true;
+	}
 
 
 
